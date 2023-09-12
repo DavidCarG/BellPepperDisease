84656418a1474b82ca4df0ce327f8edd24b0761c
David Cardenas Gonzalez <davidcardenasgonz@gmail.com>
2023-09-12 12:03:32 -0600

    Update the script for CORS

--- api/main.py
+++ api/main.py
@@ -4,8 +4,18 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:63342"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL = tf.keras.models.load_model('../saved_models/1')
 CLASS_NAMES = ['Bacterial Spot', 'Healthy']
@@ -21,17 +31,20 @@ def read_file_as_image(data) -> np.ndarray:
 async def predict(
     file: UploadFile = File(...)
 ):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
-    predictions = MODEL.predict(img_batch)
+    try:
+        image = read_file_as_image(await file.read())
+        img_batch = np.expand_dims(image, 0)
+        predictions = MODEL.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
+        predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
 
-    return {
-        'class' : predicted_class,
-        'confidence' : float(confidence)
-    }
+        return {
+            'class': predicted_class,
+            'confidence': float(confidence)
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == '__main__':
     uvicorn.run(app,host='localhost', port=8000)
